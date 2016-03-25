825f1f118ea7c579d7bcb6a09f88364526b61570
baimac <Marco_Sun@marco-suns-imac.local>
2016-03-25 11:44:03 +0800

    add a new test class.

---
+++ TestClass.h
@@ -0,0 +1,14 @@
+//
+//  TestClass.h
+//  test-unlock
+//
+//  Created by Marco_Sun on 16/3/25.
+//  Copyright © 2016年 com.60. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+@property (copy,nonatomic) NSString *testProperty;
+
+@end
---
+++ TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  test-unlock
+//
+//  Created by Marco_Sun on 16/3/25.
+//  Copyright © 2016年 com.60. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
