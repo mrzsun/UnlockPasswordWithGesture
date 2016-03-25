8e1e5cd799a624e67f5af5c909cfdde04cec0a13
baimac <Marco_Sun@marco-suns-imac.local>
2016-03-25 11:47:43 +0800

    commit a second class.

---
+++ TwoTest.h
@@ -0,0 +1,14 @@
+//
+//  TwoTest.h
+//  test-unlock
+//
+//  Created by Marco_Sun on 16/3/25.
+//  Copyright © 2016年 com.60. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TwoTest : NSObject
+@property (copy,nonatomic) NSString *twoSomething;
+
+@end
---
+++ TwoTest.m
@@ -0,0 +1,13 @@
+//
+//  TwoTest.m
+//  test-unlock
+//
+//  Created by Marco_Sun on 16/3/25.
+//  Copyright © 2016年 com.60. All rights reserved.
+//
+
+#import "TwoTest.h"
+
+@implementation TwoTest
+
+@end
